846eaa363aee379485221a6b5e027fa136ee9604
Jenny Kam <jkam17@gmail.com>
2019-08-01 17:41:59 -0400

    add about page and route

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () =>{
+  return(
+    <div>
+      <p>Data for card base stats downloaded from this spreadsheet. (Google Sheets)</p>
+      <a href='https://docs.google.com/spreadsheets/d/1XW7eyYXCsn4D7uQeqD12F-CqaBdqBxZmevoMd4kxm8o/edit#gid=1253669078'>BTS World Cards</a>
+    </div>
+
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import NavBar from './NavBar.js'
 import Calculator from './Calculator/Calculator.js'
 import LevelContainer from './LevelCalc/LevelContainer.js'
 import ByName from './ByName/Container.js'
+import About from './About.js'
 
-// function App() {
 class App extends React.Component{
   render(){
     return (
@@ -19,6 +19,8 @@ class App extends React.Component{
             <Route exact path='/oldcalc' render={()=><Calculator/>} />
             <Route exact path='/levels' render={ () => <LevelContainer />} />
             <Route exact path='/' render={ () => <ByName />} />
+            <Route exact path='/about' render={ () => <About />} />
+
           </>
         </Router>
       </div>
